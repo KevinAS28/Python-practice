60a159992a422499da7994cf70983aea40b3495a
KevinAS28 <kevinagusto28@gmail.com>
2019-09-30 14:16:42 +0700

    Initial init

---
+++ example/example.py
@@ -0,0 +1,8 @@
+"""
+Buatlah program dengan input 3 angka dipisah dengan spasi, lalu kalikan angka kedua dengan ketiga, lalu tambahkan dengan angka pertama
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a+b*c)
